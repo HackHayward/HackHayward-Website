ffe647387e2ccdfae2b06656c1f46fed8c96d91d
Thai Pham <thaiphm0303@gmail.com>
2024-08-04 19:21:48 -0700

    set up hackhayward project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+    return (
+        <>
+            <body className="box-border	m-0 p-0 bg-slate-500">
+                <h1 className="bg-zinc-800 text-slate-50 h-60 w-60 animate-jump-in animate-delay-300 animate-once">
+                    hello! this is for testing!
+                </h1>
+            </body>
+        </>
+    );
+}
+
+export default App;
