e9ddd61a4f12afca26936a0ee16479ad01afb986
Thai Pham <thaiphm0303@gmail.com>
2024-08-08 18:17:58 -0700

    Implement FAQ

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,32 +1,41 @@
 function NavButtons() {
-    return(
+    return (
         <>
             <li className="px-2">
-                <a href="#about" className="text-white text-xl font-['Roboto'] font-medium">
+                <a
+                    href="#about"
+                    className="text-white text-xl font-['Roboto'] font-medium"
+                >
                     About
                 </a>
             </li>
             <li className="px-2">
-                <a href="#faq" className="text-white text-xl font-['Roboto'] font-medium">
+                <a
+                    href="#faq"
+                    className="text-white text-xl font-['Roboto'] font-medium"
+                >
                     FAQ
                 </a>
             </li>
             <li className="px-2">
-                <a href="#sponsor" className="text-white text-xl font-['Roboto'] font-medium">
+                <a
+                    href="#sponsor"
+                    className="text-white text-xl font-['Roboto'] font-medium"
+                >
                     Sponsor Us
                 </a>
             </li>
         </>
     );
 }
-                        
+
 export default function NavBar() {
-    return(
+    return (
         <>
             <div className="navbar lg:px-16 lg:pt-16 px-6 pt-6 fixed z-fix">
                 {/* Logo */}
                 <div className="max-lg:navbar-start ">
-                    <a className="max-lg:h-20 max-sm:h-16">
+                    <a href="#home" className="max-lg:h-20 max-sm:h-16">
                         <img
                             src="./assets/Monotone Logo.png"
                             alt="HackHayward logo"
@@ -43,12 +52,21 @@ export default function NavBar() {
                 {/* Mobile Nav-Links */}
                 <div className="max-lg:navbar-end">
                     <div className="dropdown">
-                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                            <img src="../../assets/hamburger_Icon.svg" alt="Menu" className="h-8 w-8"></img>
+                        <div
+                            tabIndex={0}
+                            role="button"
+                            className="btn btn-ghost lg:hidden"
+                        >
+                            <img
+                                src="../../assets/hamburger_Icon.svg"
+                                alt="Menu"
+                                className="h-8 w-8"
+                            ></img>
                         </div>
                         <ul
-                        tabIndex={0}
-                        className="menu menu-sm menu-fix dropdown-content bg-slate-900 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                            tabIndex={0}
+                            className="menu menu-sm menu-fix dropdown-content bg-slate-900 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+                        >
                             <NavButtons />
                         </ul>
                     </div>
