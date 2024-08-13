2638fa3a78943b619f06c3257a144e35eebe0d40
Thai Pham <thaiphm0303@gmail.com>
2024-08-12 18:20:14 -0700

    v1.2 AboutUs + Animated transitions

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -2,7 +2,10 @@ export default function Hero() {
     return (
         <>
             {/* title */}
-            <div className="text-white flex flex-col lg:gap-4 gap-2 z-fix-10 font-['Exo_2']">
+            <div
+                className="text-white flex flex-col lg:gap-4 gap-2 z-fix-10 font-['Exo_2'] animate-fade-up
+"
+            >
                 <h1 className="lg:text-8xl sm:text-6xl text-5xl max-[340px]:text-4xl font-bold z-3 max-lg:text-center">
                     HackHayward
                 </h1>
@@ -15,14 +18,16 @@ export default function Hero() {
                 {/* Buttons */}
                 <div className="flex gap-4 max-lg:justify-center lg:mt-4 mt-2 font-['Space_Mono']">
                     <button
-                        className="bg-purple-500 text-white lg:h-16 lg:px-14 lg:py-4 h-12 px-6 pr-9
-                    hover:bg-purple-700 transition max-lg:text-sm slash-r"
+                        className="bg-[#c593e9] text-white lg:h-16 lg:px-14 lg:py-4 h-12 px-6 pr-9
+                    hover:bg-[#cfb0e8] transition max-lg:text-sm slash-r animate-flip-up
+"
                     >
-                        Get Notified
+                        Pre-Register
                     </button>
                     <button
                         className="border-2 border-white text-white lg:h-16 lg:px-14 lg:py-4 h-12 px-6 pl-9
-                    hover:bg-gray-700 transition max-lg:text-sm slash-l"
+                    hover:bg-gray-700 transition max-lg:text-sm slash-l animate-flip-down
+"
                     >
                         Sponsor Us
                     </button>
@@ -31,7 +36,7 @@ export default function Hero() {
             {/* Artwork */}
             <div
                 className='bg-[url("./assets/cave_S1.svg")] bg-no-repeat bg-contain bg-right-bottom
-                top-0 right-0 z-0 h-full w-full absolute mobile-fix flex justify-end overflow-hidden'
+                top-0 right-0 z-0 h-full w-full absolute mobile-fix flex justify-end overflow-hidden '
             >
                 <div
                     className='bg-[url("./assets/Astro_S1_2.0.svg")] bg-no-repeat bg-contain bg-left-bottom
