83cae0d6fe195684c1174a4a23f08419f47ff168
Eugene Vuong <vuongeugene@gmail.com>
2024-08-14 21:17:32 -0700

    Fixed Bird in Hero Section

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -2,7 +2,8 @@ export default function Hero() {
     return (
         <>
             {/* Title */}
-            <div className="bg-[#30252d] h-[calc(100vh-8rem)] grid grid-cols-10">
+
+            <div className="bg-[#30252d] min-h-[calc(100vh-8rem)] grid grid-cols-10">
                 <div className="text-white col-[2/10] row-[1] place-content-center z-50">
                     <h1 className="lg:text-8xl sm:text-6xl text-5xl max-[340px]:text-4xl font-bold z-3 max-lg:text-center">
                         HackHayward
@@ -14,13 +15,20 @@ export default function Hero() {
                         Spring 2025
                     </p>
                 </div>
-                <div className='bg-[url("./assets/HeroScene.png")] bg-no-repeat bg-contain bg-right-bottom h-full w-full col-[1/11] row-[1] relative'>
-                    <img
-                        src="./assets/Astro_S1.png"
-                        alt="mascot"
-                        className="absolute right-[10%] bottom-[10%] h-[25%] w-[25%] object-contain
+                <div className="col-[1/11] row-[1] place-self-end">
+                    <div className="relative">
+                        <img
+                            src="./assets/HeroScene.png"
+                            alt="background"
+                            className="object-contain max-h-[calc(100vh-8rem)]"
+                        />
+                        <img
+                            src="./assets/Astro_S1.png"
+                            alt="mascot"
+                            className="absolute bottom-[35%] right-[20%] h-[25%] w-[25%] object-contain
         animate-wiggle animate-infinite animate-duration-[10000ms] animate-delay-1000 animate-ease-in-out"
-                    />
+                        />
+                    </div>
                 </div>
             </div>
         </>
