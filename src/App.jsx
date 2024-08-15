f311d3cd8e6878e9e7c8cd357e9f33ccd44b6688
Eugene Vuong <vuongeugene@gmail.com>
2024-08-14 20:30:23 -0700

    Fixed Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -8,46 +8,35 @@ import Footer from './Components/Footer';
 function App() {
     return (
         <>
-            <body className="min-h-screen box-border m-0 p-0 bg-[#4948b4] overflow-x-hidden">
-                {/* page header */}
-                <header
-                    className="h-screen bg-[rgb(48,37,45)] relative"
-                    id="home"
+            <header>
+                <NavBar />
+            </header>
+            <main>
+                <section>
+                    <Hero />
+                </section>
+                {/* about us */}
+                <section
+                    className="bg-[url(./assets/Background/BBackground.png)]  bg-center bg-cover bg-no-repeat p-10"
+                    id="about"
                 >
-                    {/* navbar & title section*/}
-                    <NavBar />
-                    <div className="lg:w-1/2 flex flex-col justify-center lg:ml-16 h-full">
-                        <Hero />
-                    </div>
-                </header>
-                <main className="">
-                    {/* <span
-                        className="h-full w-full bg-cover bg-center bg-local bg-no-repeat absolute z-0
-                        brightness-50 saturate-150"
-                    ></span> */}
-                    {/* about us */}
-                    <section
-                        className="bg-[url(./assets/Background/BBackground.png)]  bg-center bg-cover bg-no-repeat p-10"
-                        id="about"
-                    >
-                        <AboutUs />
-                    </section>
-                    {/* FAQ */}
-                    <section
-                        className="flex flex-col items-center gap-12 
+                    <AboutUs />
+                </section>
+                {/* FAQ */}
+                <section
+                    className="flex flex-col items-center gap-12 p-10
                         bg-[url(./assets/Background/BBackground.png)] bg-center bg-cover bg-no-repeat"
-                        id="faq"
-                    >
-                        <FAQ />
-                    </section>
-                    {/* sponsor */}
-                    <section></section>
-                </main>
-                {/* footer */}
-                <footer className="bg-[rgb(48,37,45)]">
-                    <Footer />
-                </footer>
-            </body>
+                    id="faq"
+                >
+                    <FAQ />
+                </section>
+                {/* sponsor */}
+                <section></section>
+            </main>
+            {/* footer */}
+            <footer className="bg-[rgb(48,37,45)]">
+                <Footer />
+            </footer>
         </>
     );
 }
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -2,45 +2,27 @@ export default function Hero() {
     return (
         <>
             {/* Title */}
-            <header className="text-white flex flex-col lg:gap-4 gap-2 z-fix-10 font-exo2 animate-fade-up">
-                <h1 className="lg:text-8xl sm:text-6xl text-5xl max-[340px]:text-4xl font-bold z-3 max-lg:text-center">
-                    HackHayward
-                </h1>
-                <p className="lg:text-4xl text-2xl mt-2 max-lg:text-center">
-                    In-Person @ CSU East Bay
-                </p>
-                <p className="lg:text-4xl text-2xl max-lg:text-center">
-                    Spring 2025
-                </p>
-                {/* Buttons */}
-                <nav className="flex gap-4 max-lg:justify-center lg:mt-4 mt-2 font-mono">
-                    <button
-                        className="bg-[#c593e9] text-white lg:h-16 lg:px-14 lg:py-4 h-12 px-6 pr-9
-                    hover:bg-[#cfb0e8] transition max-lg:text-sm slash-r animate-flip-up"
-                    >
-                        Pre-Register
-                    </button>
-
-                    <button
-                        className="border-2 border-white text-white lg:h-16 lg:px-14 lg:py-4 h-12 px-6 pl-9
-                    hover:bg-gray-700 transition max-lg:text-sm slash-l animate-flip-down"
-                    >
-                        Sponsor Us
-                    </button>
-                </nav>
-            </header>
-            {/* Artwork */}
-            <aside
-                className='bg-[url("./assets/New.cave_3.0sl.png")] bg-no-repeat bg-contain bg-right-bottom
-                top-0 right-0 z-0 h-full w-full absolute mobile-fix flex justify-end overflow-hidden '
-            >
-                <img
-                    src="./assets/Astro_S1_2.0.svg"
-                    alt="mascot"
-                    className="
-                    top-0 right-0 w-1/2 animate-wiggle animate-infinite animate-duration-[10000ms] animate-delay-1000 animate-ease-in-out"
-                />
-            </aside>
+            <div className="bg-[#30252d] h-[calc(100vh-8rem)] grid grid-cols-10">
+                <div className="text-white col-[2/10] row-[1] place-content-center z-50">
+                    <h1 className="lg:text-8xl sm:text-6xl text-5xl max-[340px]:text-4xl font-bold z-3 max-lg:text-center">
+                        HackHayward
+                    </h1>
+                    <p className="lg:text-4xl text-2xl mt-2 max-lg:text-center">
+                        In-Person @ CSU East Bay
+                    </p>
+                    <p className="lg:text-4xl text-2xl max-lg:text-center">
+                        Spring 2025
+                    </p>
+                </div>
+                <div className='bg-[url("./assets/HeroScene.png")] bg-no-repeat bg-contain bg-right-bottom h-full w-full col-[1/11] row-[1] relative'>
+                    <img
+                        src="./assets/Astro_S1.png"
+                        alt="mascot"
+                        className="absolute right-[10%] bottom-[10%] h-[25%] w-[25%] object-contain
+        animate-wiggle animate-infinite animate-duration-[10000ms] animate-delay-1000 animate-ease-in-out"
+                    />
+                </div>
+            </div>
         </>
     );
 }
