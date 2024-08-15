20f3b0833a4485bea4496a6767d8181c4f015ddc
Eugene Vuong <vuongeugene@gmail.com>
2024-08-15 14:25:03 -0700

    Added Scene5

---
+++ src/Components/Scene5.jsx
@@ -0,0 +1,14 @@
+export default function Scene5() {
+    return (
+        <div className="max-h-[50vh] relative">
+            <img
+                className="object-fill w-full"
+                src="/assets/Scene5/The_eascape_background_s5.png"
+            />
+            <img
+                src="/assets/Scene5/SpaceShip.png"
+                className="absolute z-50 top-[10%] left-[10%] h-[85%] w-[85%] object-contain"
+            />
+        </div>
+    );
+}
