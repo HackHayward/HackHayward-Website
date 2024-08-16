1dfcab8767e289a9e006d929999b66f9e375c713
Thai Pham <thaiphm0303@gmail.com>
2024-08-16 00:15:53 -0700

    fix banner issues

--- src/Components/MLH.jsx
+++ src/Components/MLH.jsx
@@ -1,7 +1,7 @@
 export default function MLH() {
     return (
         <>
-            <div className=" absolute top-[0%] lg:right-[2%] right-[22%] lg:h-[8%] lg:w-[8%] h-[20%] w-[20%] z-[200] ">
+            <div className=" absolute top-[0%] lg:right-[2%] right-[22%] lg:h-[8%] lg:w-[8%] h-[20%] w-[20%] lg:z-[200] ">
                 <a href="https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2025-season&utm_content=white ">
                     <img
                         src="./assets/sponsors/MLH1.svg"
