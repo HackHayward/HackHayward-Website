ad7b49573072a79e241b309e28b29f75b696c4d6
Thai Pham <thaiphm0303@gmail.com>
2024-08-15 21:34:37 -0700

    Add link to MLH

--- src/Components/MLH.jsx
+++ src/Components/MLH.jsx
@@ -1,8 +1,10 @@
 export default function MLH() {
     return (
         <>
-            <div className="hidden absolute top-[0%] lg:right-[2%] right-[22%] lg:h-[8%] lg:w-[8%] h-[20%] w-[20%] z-30">
-                <img src="./assets/sponsors/MLH1.svg" alt="banner" />
+            <div className=" absolute top-[0%] lg:right-[2%] right-[22%] lg:h-[8%] lg:w-[8%] h-[20%] w-[20%] z-30">
+                <a href="https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2025-season&utm_content=white">
+                    <img src="./assets/sponsors/MLH1.svg" alt="banner" />
+                </a>
             </div>
         </>
     );
