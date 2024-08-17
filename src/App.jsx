380aaa198b7d800bb0b404b5862bb170717f4984
Eugene Vuong <vuongeugene@gmail.com>
2024-08-17 02:41:48 -0700

    Fixed Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -10,35 +10,38 @@ import Sponsor from './Components/Sponsor';
 function App() {
     return (
         <>
-            <header id="navbar">
+            <header>
                 <NavBar />
             </header>
-            <main
-                className="bg-[url(./assets/Background/BBackground.png)] bg-contain bg-repeat-y overflow-hidden"
-                id="home"
-            >
+            <main className="bg-[url(./assets/Background/BBackground.png)] bg-contain bg-repeat-y">
                 <section className="border-b-8 border-[#c593e9]">
                     <Hero />
                 </section>
                 {/* about us */}
-                <section className="p-10 bg-black/50" id="about">
+                <section
+                    className="p-10 bg-black/50 overflow-hidden"
+                    id="about"
+                >
                     <AboutUs />
                 </section>
                 {/* FAQ */}
-                <section className="p-10 bg-black/50" id="faq">
+                <section className="p-10 bg-black/50 overflow-hidden" id="faq">
                     <FAQ />
                 </section>
                 {/* sponsor */}
-                <section className="p-10 bg-black/50" id="sponsor-us">
+                <section
+                    className="p-10 bg-black/50 overflow-hidden"
+                    id="sponsor-us"
+                >
                     <Sponsor />
                 </section>
                 {/* Scene */}
-                <section>
+                <section className="overflow-hidden">
                     <PilotFalcon />
                 </section>
             </main>
             {/* footer */}
-            <footer className="bg-[rgb(48,37,45)] border-t-8 border-[#c593e9]">
+            <footer className="bg-[rgb(48,37,45)] border-t-8 border-[#c593e9] overflow-hidden">
                 <Footer />
             </footer>
         </>
