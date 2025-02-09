72b96ddd8ae00c66745f6d20abd641e470f2c39c
1meza <billyboe325@gmail.com>
2025-02-09 02:25:57 -0800

    fixed rabbit potential issue

--- src/Components/CountdownTimer.jsx
+++ src/Components/CountdownTimer.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-//import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 const CountdownTimer = ({ targetDate }) => {
     const [timeLeft, setTimeLeft] = useState({
